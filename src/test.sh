48d7b87ea2043d3f1a6f4086e71f749323723217
Julian Mateu <julianmateu@gmail.com>
2016-01-27 12:28:01 -0300

    Added test from file
    Added a TestQuaternionDataFile test, to run test cases from files.
    Added a file named testdata.txt with some tests I could think about.

--- src/test.sh
+++ src/test.sh
@@ -1,2 +1,3 @@
-javac Quaternion.java TestQuaternion.java
+javac Quaternion.java TestQuaternion.java TestQuaternionDataFile.java
 java junit.textui.TestRunner TestQuaternion
+java junit.textui.TestRunner TestQuaternionDataFile
