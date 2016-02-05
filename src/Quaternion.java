4c074ccb6a02be96fe253728b10b400bfd7a4246
Julian Mateu <julianmateu@gmail.com>
2016-02-05 11:01:19 -0300

    Add methods to subtract and multiply
    -Add `subtract()` and `multiply()` methods in Quaternion.java, but no tests.

--- src/Quaternion.java
+++ src/Quaternion.java
@@ -7,7 +7,7 @@
  * @version 1.0
  */
 public class Quaternion {
-    //fields
+    //************ fields ************//
     private double r, i, j, k;
 
     /** Constructor
@@ -20,52 +20,43 @@ public class Quaternion {
         this.k = k;
     }
 
-    // Getters & Setters
-    
-    /** r Getter
-     */
-    public double getR() {return this.r;}
-
-    /** i Getter
-     */
-    public double getI() {return this.i;}
-
-
-    /** j Getter
-     */
-    public double getJ() {return this.j;}
-
-    /** k Getter
-     */
-    public double getK() {return this.k;}
-
-    /** r Setter
-     */
-    public void setR(double r) {this.r = r;}
-
-    /** i Setter
-     */
-    public void setI(double i) {this.i = i;}
-
-    /** j Setter
-     */
-    public void setJ(double j) {this.j = j;}
-
-    /** k Setter
-     */
-    public void setK(double k) {this.k = k;}
-
-    // Methods
+    //************ Methods ************//
 
     /** Method to add two Quaternions
      * @return It returns a new Quaternion with the result of the sum.
      */
     public Quaternion add(Quaternion addend) {
         return new Quaternion(
-            this.r + addend.r,
-            this.i + addend.i,
-            this.j + addend.j,
-            this.k + addend.k);
+            this.r + addend.getR(),
+            this.i + addend.getI(),
+            this.j + addend.getJ(),
+            this.k + addend.getK());
+    }
+
+    /** Method to subtract two Quaternions
+     * @return It returns a new Quaternion with the result of the subtraction.
+     */
+    public Quaternion subtract(Quaternion subtrahend) {
+        return new Quaternion(
+                this.r - subtrahend.getR(),
+                this.i - subtrahend.getI(),
+                this.j - subtrahend.getJ(),
+                this.k - subtrahend.getK());
+    }
+
+    /** Method to multiply two Quaternions and find their Hamilton product.
+     * @return Returns a new Quaternion with the result of the Hamilton product.
+     */
+    public Quaternion multiply(Quaternion factor) {
+        return new Quaternion(
+                this.r * factor.getR() - this.i * factor.getI()
+                - this.j * factor.getJ() - this.k * factor.getK(),
+                this.r * factor.getI() + this.i * factor.getR()
+                + this.j * factor.getK() - this.k * factor.getJ(),
+                this.r * factor.getJ() - this.i * factor.getK()
+                + this.j * factor.getR() + this.k * factor.getI(),
+                this.r * factor.getK() + this.i * factor.getJ()
+                - this.j * factor.getI() + this.k * factor.getR());
     }
 
     /** Method to assert if to Quaternions are equal.
@@ -90,5 +81,56 @@ public class Quaternion {
     @Override
     public String toString(){
         return "( " + this.r + " ; " + this.i + " ; " + this.j + " ; " + this.k + " )";
+    }
+ 
+    //************ Getters & Setters ************//
+    
+    /** r Getter
+     */
+    public double getR() {
+        return this.r;
+    }
+
+    /** i Getter
+     */
+    public double getI() {
+        return this.i;
+    }
+
+
+    /** j Getter
+     */
+    public double getJ() {
+        return this.j;
+    }
+
+    /** k Getter
+     */
+    public double getK() {
+        return this.k;
+    }
+
+    /** r Setter
+     */
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    /** i Setter
+     */
+    public void setI(double i) {
+        this.i = i;
+    }
+
+    /** j Setter
+     */
+    public void setJ(double j) {
+        this.j = j;
+    }
+
+    /** k Setter
+     */
+    public void setK(double k) {
+        this.k = k;
     }
 } ///:~
