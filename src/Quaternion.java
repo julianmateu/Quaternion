9e00b32342980ce5f9a59bd44c87034e0322162d
Julian Mateu <julianmateu@gmail.com>
2016-02-05 15:15:38 -0300

    Add method to multiply by scalar and tests
    -Added a method to multiply by scalar.
    -Added tests for subtract, and multiply methods.
    
    The multiply by quaternion method has problems with tolerance.

--- src/Quaternion.java
+++ src/Quaternion.java
@@ -1,7 +1,9 @@
 //: object/Quaternion.java
 
-/** A Java Class to operate with Quaternions.
+/**
+ * A Java Class to operate with Quaternions.
  * Read more about Quaternions <a href="https://en.wikipedia.org/wiki/Quaternion">here</a>
+ *
  * @author Julian Mateu
  * @author julianmateu@gmail.com
  * @version 1.0
@@ -10,8 +12,10 @@ public class Quaternion {
     //************ fields ************//
     private double r, i, j, k;
 
-    /** Constructor
-     * @param r Real part
+    /**
+     * Constructor.
+     *
+     * @param r Real part.
      */
     public Quaternion(double r, double i, double j, double k) {
         this.r = r;
@@ -22,18 +26,22 @@ public class Quaternion {
 
     //************ Methods ************//
 
-    /** Method to add two Quaternions
+    /**
+     * Method to add two Quaternions.
+     *
      * @return It returns a new Quaternion with the result of the sum.
      */
     public Quaternion add(Quaternion addend) {
         return new Quaternion(
-            this.r + addend.getR(),
-            this.i + addend.getI(),
-            this.j + addend.getJ(),
-            this.k + addend.getK());
+                this.r + addend.getR(),
+                this.i + addend.getI(),
+                this.j + addend.getJ(),
+                this.k + addend.getK());
     }
 
-    /** Method to subtract two Quaternions
+    /**
+     * Method to subtract two Quaternions.
+     *
      * @return It returns a new Quaternion with the result of the subtraction.
      */
     public Quaternion subtract(Quaternion subtrahend) {
@@ -44,30 +52,50 @@ public class Quaternion {
                 this.k - subtrahend.getK());
     }
 
-    /** Method to multiply two Quaternions and find their Hamilton product.
+    /**
+     * Method to multiply two Quaternions and find their Hamilton product.
+     *
      * @return Returns a new Quaternion with the result of the Hamilton product.
      */
     public Quaternion multiply(Quaternion factor) {
         return new Quaternion(
                 this.r * factor.getR() - this.i * factor.getI()
-                - this.j * factor.getJ() - this.k * factor.getK(),
+                        - this.j * factor.getJ() - this.k * factor.getK(),
                 this.r * factor.getI() + this.i * factor.getR()
-                + this.j * factor.getK() - this.k * factor.getJ(),
+                        + this.j * factor.getK() - this.k * factor.getJ(),
                 this.r * factor.getJ() - this.i * factor.getK()
-                + this.j * factor.getR() + this.k * factor.getI(),
+                        + this.j * factor.getR() + this.k * factor.getI(),
                 this.r * factor.getK() + this.i * factor.getJ()
-                - this.j * factor.getI() + this.k * factor.getR());
+                        - this.j * factor.getI() + this.k * factor.getR());
     }
 
-    /** Method to assert if to Quaternions are equal.
+    /**
+     * Method to multiply a Quaternion by a scalar.
+     *
+     * @return Returns a new Quaternion with the result of the product by scalar.
+     */
+    public Quaternion multiply(double scalar) {
+        return new Quaternion(
+                this.r * scalar,
+                this.i * scalar,
+                this.j * scalar,
+                this.k * scalar);
+    }
+
+    /**
+     * Method to assert if to Quaternions are equal.
+     *
      * @return Returns True if they are equal, and False otherwise.
      */
     public boolean equals(Quaternion compared) {
-        
-        return this.r == compared.getR() && this.i == compared.getI() && this.j == compared.getJ() && this.k == compared.getK();
+
+        return this.r == compared.getR() && this.i == compared.getI() && this.j ==
+                compared.getJ() && this.k == compared.getK();
     }
 
-    /** Method to assert if to Quaternions are equal.
+    /**
+     * Method to assert if to Quaternions are equal.
+     *
      * @return Returns True if they are equal, and False otherwise.
      */
     @Override
@@ -75,60 +103,70 @@ public class Quaternion {
         return compared instanceof Quaternion && this.equals((Quaternion) compared);
     }
 
-    /** Method to convert a Quaternion to a string.
+    /**
+     * Method to convert a Quaternion to a string.
+     *
      * @return Returns the Quaternion in the form ( r ; i ; j ; k ).
      */
     @Override
-    public String toString(){
+    public String toString() {
         return "( " + this.r + " ; " + this.i + " ; " + this.j + " ; " + this.k + " )";
     }
- 
+
     //************ Getters & Setters ************//
-    
-    /** r Getter
+
+    /**
+     * r Getter.
      */
     public double getR() {
         return this.r;
     }
 
-    /** i Getter
+    /**
+     * i Getter.
      */
     public double getI() {
         return this.i;
     }
 
 
-    /** j Getter
+    /**
+     * j Getter.
      */
     public double getJ() {
         return this.j;
     }
 
-    /** k Getter
+    /**
+     * k Getter.
      */
     public double getK() {
         return this.k;
     }
 
-    /** r Setter
+    /**
+     * r Setter.
      */
     public void setR(double r) {
         this.r = r;
     }
 
-    /** i Setter
+    /**
+     * i Setter.
      */
     public void setI(double i) {
         this.i = i;
     }
 
-    /** j Setter
+    /**
+     * j Setter.
      */
     public void setJ(double j) {
         this.j = j;
     }
 
-    /** k Setter
+    /**
+     * k Setter.
      */
     public void setK(double k) {
         this.k = k;
--- src/TestQuaternion.java
+++ src/TestQuaternion.java
@@ -1,49 +1,170 @@
 //: object/TestQuaternion.java
+
 import junit.framework.*;
 
-/** JUnit Class to test the methods from the Quaternion Class.
+/**
+ * JUnit Class to test the methods from the Quaternion Class.
+ *
  * @author Julian Mateu
  * @author julianmateu@gmail.com
  * @version 1.0
  */
 public class TestQuaternion extends TestCase {
 
-    /** Constructor
+    /**
+     * Constructor
      */
     public TestQuaternion(String name) {
         super(name);
     }
 
-    /** Tester for the equals() method.
+    /**
+     * Tester for the equals() method.
      */
     public void testEquals() {
-        Quaternion a = new Quaternion(1,0,-1,5);
+        Quaternion a = new Quaternion(1, 0, -1, 5);
 
-        assertEquals(a,new Quaternion(1,0,-1,5));
-        assertEquals(a,a);
-        assertFalse(a.equals(new Quaternion(-1,0,0,5)));
+        assertEquals(a, new Quaternion(1, 0, -1, 5));
+        assertEquals(a, a);
+        assertFalse(a.equals(new Quaternion(-1, 0, 0, 5)));
 
     }
 
-    /** Tester for the add() method.
+    /**
+     * Tester for the add() method.
      */
     public void testAddition() {
 
         // Simple tests:
-        assertEquals(new Quaternion(1,2,3,4).add(new Quaternion(1,1,1,1)),new Quaternion(2,3,4,5));
-        assertEquals(new Quaternion(1,1,1,1).add(new Quaternion(0,0,0,0)),new Quaternion(1,1,1,1)); // Adding Identity element.
+        assertEquals(new Quaternion(1, 2, 3, 4).add(new Quaternion(1, 1, 1, 1)),
+                new Quaternion(2, 3, 4, 5));
+        assertEquals(new Quaternion(1, 1, 1, 1).add(new Quaternion(0, 0, 0, 0)),
+                new Quaternion(1, 1, 1, 1)); // Adding Identity element.
 
         // Negative number tests:
-        assertEquals(new Quaternion(-1,-1,-1,-1).add(new Quaternion(0,0,0,0)),new Quaternion(-1,-1,-1,-1));
-        assertEquals(new Quaternion(1,1,1,1).add(new Quaternion(-1,-1,-1,-1)),new Quaternion(0,0,0,0));
-        assertEquals(new Quaternion(-1,-1,-1,-1).add(new Quaternion(1,1,1,1)),new Quaternion(0,0,0,0)); // Commutativity
-        assertEquals(new Quaternion(-1,-1,-1,-1).add(new Quaternion(-1,-1,-1,-1)),new Quaternion(-2,-2,-2,-2));
+        assertEquals(new Quaternion(-1, -1, -1, -1).add(new Quaternion(0, 0, 0, 0)),
+                new Quaternion(-1, -1, -1, -1));
+        assertEquals(new Quaternion(1, 1, 1, 1).add(new Quaternion(-1, -1, -1, -1)),
+                new Quaternion(0, 0, 0, 0)); // Adding additive inverse.
+        assertEquals(new Quaternion(-1, -1, -1, -1).add(new Quaternion(1, 1, 1, 1)),
+                new Quaternion(1, 1, 1, 1).add(new Quaternion(-1, -1, -1, -1))); // Commutativity.
+        assertEquals(new Quaternion(-1, -1, -1, -1).add(new Quaternion(-1, -1, -1, -1)),
+                new Quaternion(-2, -2, -2, -2));
 
         // Mixed numbers tests:
-        assertEquals(new Quaternion(1,1,1,-1).add(new Quaternion(1,0,-1,-1)),new Quaternion(2,1,0,-2));
-        assertEquals(new Quaternion(-1,4,70,-1).add(new Quaternion(-9,-4,30,4)),new Quaternion(-10,0,100,3));
-        
+        assertEquals(new Quaternion(1, 1, 1, -1).add(new Quaternion(1, 0, -1, -1)),
+                new Quaternion(2, 1, 0, -2));
+        assertEquals(new Quaternion(-1, 4, 70, -1).add(new Quaternion(-9, -4, 30, 4)),
+                new Quaternion(-10, 0, 100, 3));
+
         // Floating point tests:
-        assertEquals(new Quaternion(1.25,1e10,1.234e-1,-1).add(new Quaternion(1.25,2e9,-1.234e-1,-1)),new Quaternion(2.5,1.2e10,0,-2));
+        assertEquals(new Quaternion(1.25, 1e10, 1.234e-1, -1).add(
+                new Quaternion(1.25, 2e9, -1.234e-1, -1)), new Quaternion(2.5, 1.2e10, 0, -2));
+    }
+
+
+    /**
+     * Tester for the subtract() method.
+     */
+    public void testSubtraction() {
+
+        // Simple tests:
+        assertEquals(new Quaternion(1, 2, 3, 4).subtract(new Quaternion(1, 1, 1, 1)),
+                new Quaternion(0, 1, 2, 3));
+        assertEquals(new Quaternion(1, 1, 1, 1).subtract(new Quaternion(0, 0, 0, 0)),
+                new Quaternion(1, 1, 1, 1)); // Subtracting Identity element.
+        assertEquals(new Quaternion(1, 2, 3, 4).subtract(new Quaternion(1, 2, 3, 4)),
+                new Quaternion(0, 0, 0, 0));
+
+        // Negative number tests:
+        assertEquals(new Quaternion(0, 0, 0, 0).subtract(new Quaternion(1, 1, 1, 1)),
+                new Quaternion(-1, -1, -1, -1)); // Inverse.
+        assertEquals(new Quaternion(-1, -1, -1, -1).subtract(new Quaternion(-1, -1, -1, -1)),
+                new Quaternion(0, 0, 0, 0));
+        assertEquals(new Quaternion(1, 1, 1, 1).subtract(new Quaternion(-1, -1, -1, -1)),
+                new Quaternion(2, 2, 2, 2));
+        assertEquals(new Quaternion(-1, -1, -1, -1).subtract(new Quaternion(1, 1, 1, 1)),
+                new Quaternion(-2, -2, -2, -2)); // Anticommutativity.
+
+        // Mixed numbers tests:
+        assertEquals(new Quaternion(1, 1, 1, -1).subtract(new Quaternion(1, 0, -1, 1)),
+                new Quaternion(0, 1, 2, -2));
+        assertEquals(new Quaternion(-1, 4, 70, -1).subtract(new Quaternion(9, 4, -30, -4)),
+                new Quaternion(-10, 0, 100, 3));
+
+        // Floating point tests:
+        assertEquals(new Quaternion(1.25, 1e10, 1.234e-1, -1).subtract(
+                new Quaternion(-1.25, -2e9, 1.234e-1, 1)), new Quaternion(2.5, 1.2e10, 0, -2));
+    }
+
+    /**
+     * Tester for the multiply(double) method.
+     */
+    public void testMultiplicationDouble() {
+
+        // Simple tests:
+        assertEquals(new Quaternion(1, 2, 3, 4).multiply(2),
+                new Quaternion(2, 4, 6, 8));
+        assertEquals(new Quaternion(1, 2, 3, 4).multiply(1),
+                new Quaternion(1, 2, 3, 4)); // Multiplying by identity element.
+        assertEquals(new Quaternion(1, 1, 1, 1).multiply(4.0),
+                new Quaternion(4, 4, 4, 4));
+        assertEquals(new Quaternion(1, 1, 1, 1).multiply(0.0),
+                new Quaternion(0, 0, 0, 0)); // Multiplying by zero scalar.
+        assertEquals(new Quaternion(0, 0, 0, 0).multiply(1.0),
+                new Quaternion(0, 0, 0, 0)); // Multiplying by zero quaternion.
+        assertEquals(new Quaternion(2, 2, 2, 2).multiply(0.5),
+                new Quaternion(1, 1, 1, 1)); // Multiplying by inverse of each element.
+
+
+        // Negative number tests:
+        assertEquals(new Quaternion(-1, -1, -1, -1).multiply(1),
+                new Quaternion(-1, -1, -1, -1));
+        assertEquals(new Quaternion(1, 1, 1, 1).multiply(-1),
+                new Quaternion(-1, -1, -1, -1));
+        assertEquals(new Quaternion(-1, -1, -1, -1).multiply(-1),
+                new Quaternion(1, 1, 1, 1));
+
+
+        // Mixed numbers tests:
+        assertEquals(new Quaternion(-1, 2, 3, -4).multiply(2),
+                new Quaternion(-2, 4, 6, -8));
+        assertEquals(new Quaternion(-1, 4, 70, -4).multiply(-3),
+                new Quaternion(3, -12, -210, 12));
+
+        // Floating point tests:
+        assertEquals(new Quaternion(1e1, 1e10, 1.234e-1, -1).multiply(1e10),
+                new Quaternion(1e11, 1e20, 1.234e9, -1e10));
+        assertEquals(new Quaternion(1e1, 1e10, 1.234e-1, -1).multiply(-1e10),
+                new Quaternion(-1e11, -1e20, -1.234e9, 1e10));
+    }
+
+    /**
+     * Tester for the multiply(Quaternion) method.
+     */
+    public void testMultiplicationQuaternion() {
+
+        // Simple tests:
+        assertEquals(new Quaternion(1, 2, 3, 4).multiply(new Quaternion(2, 2, 2, 2)),
+                new Quaternion(-16, 4, 12, 8));
+        assertEquals(new Quaternion(1, 2, 3, 4).multiply(new Quaternion(1, 2, 3, 4)),
+                new Quaternion(-28, 4, 6, 8));
+        assertEquals(new Quaternion(1, 1, 1, 1).multiply(new Quaternion(0, 0, 0, 0)),
+                new Quaternion(0, 0, 0, 0)); // Multiplying by right zero element.
+        assertEquals(new Quaternion(0, 0, 0, 0).multiply(new Quaternion(1, 1, 1, 1)),
+                new Quaternion(0, 0, 0, 0)); // Multiplying by left zero element.
+        assertEquals(new Quaternion(1, 2, 3, 4).multiply(new Quaternion(
+                        ((double)1.0)/28, ((double)-2.0)/28, ((double)-3.0)/28, ((double)-4.0)/28)),
+                new Quaternion(1, 0, 0, 0),1e-10); // Multiplying by right inverse.
+        assertEquals(new Quaternion(
+                (double)1.0/28, (double)-2.0/28, (double)-3.0/28, (double)-4.0/28)).multiply(
+                new Quaternion(1, 2, 3, 4)),new Quaternion(1, 0, 0, 0),
+                1e-10); // Multiplying by left inverse.
+
+
+        // Negative number tests:
+        assertEquals(new Quaternion(-1, -1, -1, -1).multiply(new Quaternion(1, 2, 3, 4)),
+                new Quaternion(8, -4, -2, -6));
+
     }
 } ///:~
